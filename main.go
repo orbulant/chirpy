9ec9e294399b23873e70cf610ea3369d18c1ed8c
orbulant <vydranix@gmail.com>
2025-07-08 23:28:31 +0800

    Added tutorial chapter

--- main.go
+++ main.go
@@ -3,21 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	indexFile := http.FileServer(http.Dir("."))
+	apiCfg := apiConfig{}
+
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 
 	mux.Handle("/", indexFile)
-	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app", apiCfg.middlewareMetricsInc(handler))
 	mux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		// The line below is not needed as it automatically does it on first .Write
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.WriteHeader(http.StatusOK)
+		buf := []byte{}
+		buf = fmt.Appendf(buf, "Hits: %d", apiCfg.fileserverHits.Load())
+		w.Write(buf)
+	})
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
 	})
 
 	server := http.Server{
